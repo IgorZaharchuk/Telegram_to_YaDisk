0145d5151464b4068b86cdc683f0a94fbcb59750
IgorZaharchuk <zaharchuk.igor@gmail.com>
2026-04-25 04:32:36 +0300

    Заголовки версий в комментариях → 0.17.18

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Telegram MTProto Backup to Yandex Disk - ОРКЕСТРАТОР
-ВЕРСИЯ 0.17.1 — ИСПРАВЛЕНИЯ: HEALTHCHECKER ПРОВЕРЯЕТ RECONNECT
+ВЕРСИЯ 0.17.18 — ИСПРАВЛЕНИЯ: HEALTHCHECKER ПРОВЕРЯЕТ RECONNECT
 """
 
 __version__ = "0.17.18"
